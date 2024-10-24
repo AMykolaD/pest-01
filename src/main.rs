215addf1ef02aa8e79bb914ba656c3d6dd2439f9
AMykolaD <andrusenkomykola@gmail.com>
2024-10-24 17:51:43 +0200

    Project bugs fixed

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,11 @@
-fn main() {}
+use pest::Parser;
+use pest_derive::Parser;
+
+#[derive(Parser)]
+#[grammar = "./grammar.pest"]
+pub struct Grammar;
+fn main() -> anyhow::Result<()> {
+    let got = Grammar::parse(Rule::file, "-273.15,-15\n")?;
+    println!("{:?}", got);
+    Ok(())
+}
